e61bdf21f597246dfb1efe7d95851f16f49b3d6e
Richard Xie <richardok24@gmail.com>
2019-04-17 21:57:01 -0700

    Changed var to let and fixed format

--- 2-Add-Two-Numbers/AddTwoNumbers.js
+++ 2-Add-Two-Numbers/AddTwoNumbers.js
@@ -12,24 +12,24 @@
  */
 var addTwoNumbers = function(l1, l2) {
     
-    var List = new ListNode(0);
-    var head = List;
-    var sum = 0;
-    var carry = 0;
+    let List = new ListNode(0);
+    let head = List;
+    let sum = 0;
+    let carry = 0;
 
-    while(l1!==null||l2!==null||sum>0){
+    while(l1 !== null || l2 !==null || sum > 0){
 
-        if(l1!==null){
+        if(l1 !== null) {
             sum = sum + l1.val;
             l1 = l1.next;
         }
         
-        if(l2!==null){
+        if(l2 !== null) {
             sum = sum + l2.val;
             l2 = l2.next;
         }
         
-        if(sum>=10){
+        if(sum >= 10) {
             carry = 1;
             sum = sum - 10;
         }
