be881985c2fd8ba83b6d2b09a96e0bcfd660db56
Richard Xie <richardok24@gmail.com>
2019-06-29 11:31:25 -0700

    392 added

---
+++ 392-Is-Subsequence/Is-Subsequence.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isSubsequence = function(s, t) {
+    let i = 0, j = 0;
+    
+    while (i < s.length && j < t.length) {
+        if (s[i] == t[j]) {
+            i++;
+            j++;
+        } else {
+            j++;
+        }
+    }
+
+    if (i == s.length) {
+        return true;
+    }
+    
+    return false;
+};
