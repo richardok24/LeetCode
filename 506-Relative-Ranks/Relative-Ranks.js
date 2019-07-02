f360f80f1f4b58ca4868b8c22683d8403055e26c
Richard Xie <richardok24@gmail.com>
2019-07-02 11:40:48 -0700

    506 added

---
+++ 506-Relative-Ranks/Relative-Ranks.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @return {string[]}
+ */
+var findRelativeRanks = function(nums) {
+    let result = [];
+    let sortedArray = nums.slice();
+    sortedArray.sort((a,b) => (b-a));
+    
+    for(let i = 0; i < nums.length; i++) {
+        const j = sortedArray.indexOf(nums[i]);
+        
+        if(j === 0) {
+            result.push("Gold Medal");
+        } else if(j === 1) {
+            result.push("Silver Medal");
+        } else if(j === 2) {
+            result.push("Bronze Medal");
+        } else {
+            result.push((j + 1).toString());
+        }
+    }
+    
+    return result;
+};
